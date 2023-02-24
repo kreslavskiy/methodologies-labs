37e2942b910a67a7f345a40ede73566d02f2dce2
kreslavskiy <mishak1000236@gmail.com>
2023-02-25 01:09:45 +0200

    Add interactive mode
    Added interactive mode where all numbers have to be entered in console

---
+++ 1-lab/interactive.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '> ',
+});
+
+rl.prompt();
+
+const question = (str) => new Promise((answer) => rl.question(str, answer));
+
+(async () => {
+  const a = Number(await question('Enter a: '));
+  const b = Number(await question('Enter b: '));
+  const c = Number(await question('Enter c: '));
+
+  if (a === 0 || isNaN(a) || isNaN(b) || isNaN(c)) console.log('Error. Expected a valid real number, got NaN instead');
+  else {
+    console.log(`Equation is: ${a}x^2 + ${b}x + ${c} = 0`);
+    const discriminant = Math.pow(b, 2) - 4 * a * c;
+    if (discriminant > 0) {
+      const x1 = (-1*b + Math.sqrt(discriminant)) / (2 * a);
+      const x2 = (-1*b - Math.sqrt(discriminant)) / (2 * a);
+      console.log(`x1: ${x1}, x2: ${x2}`);
+    } else if (discriminant === 0) {
+      console.log(`Found 1 root: x = ${ (-1*b) / (2*a) }`);
+    } else {
+      console.log('There are 0 roots');
+    }
+  }
+})();
