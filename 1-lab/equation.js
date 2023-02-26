3f50c06f854a7f64c80d44ba92e32b6227388ea7
Mykhailo Kreslavskyi <89911844+kreslavskiy@users.noreply.github.com>
2023-02-26 20:54:37 +0200

    Improved equation output

--- 1-lab/equation.js
+++ 1-lab/equation.js
@@ -13,7 +13,7 @@ const FILENAME_POSITION = 3;
 
   if (!isValid) console.log('One of number is not valid. Please enter valid numbers\nNote: a, b and c have to be numbers, a must not be zero');
   else {
-    console.log(`Equation is: ${a}x^2 + ${b}x ${ c >= 0 ? '+' : '-' } ${Math.abs(c)} = 0`);
+    console.log(`Equation is: { a >= 0 ? '' : '-' } ${Math.abs(a)}x^2 ${ b >= 0 ? '+' : '-' } ${Math.abs(b)}x ${ c >= 0 ? '+' : '-' } ${Math.abs(c)} = 0`);
     solveEquation(a, b, c);
   }
 })();
