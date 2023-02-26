ff659c936bb6527559feb3f9ca2012ee00db6359
Mykhailo Kreslavskyi <89911844+kreslavskiy@users.noreply.github.com>
2023-02-26 14:47:56 +0200

    Improve validation
    Added check if a is not 0

--- 1-lab/src/validator.js
+++ 1-lab/src/validator.js
@@ -2,7 +2,7 @@
 
 const validator = (a, b, c) => {
   const numbers = [ a, b, c ];
-  return numbers.every((num => !isNaN(num)));
+  return numbers.every((num => !isNaN(num))) && a !== 0;
 };
 
 module.exports = validator;
