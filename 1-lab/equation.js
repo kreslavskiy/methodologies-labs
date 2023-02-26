0102555689186f0d5de745945f8f093078b6565e
Mykhailo Kreslavskyi <89911844+kreslavskiy@users.noreply.github.com>
2023-02-26 14:49:59 +0200

    Improve error output
    Added note in error message about what a, b and c have to be

--- 1-lab/equation.js
+++ 1-lab/equation.js
@@ -11,7 +11,7 @@ const FILENAME_POSITION = 3;
   const { a, b, c } = process.argv.length === FILENAME_POSITION ? getNumbersFromFile(process.argv[2]) : await getNumbersFromConsole();
   const isValid = validator(a, b, c);
 
-  if (!isValid) console.log('One of number is not valid. Please enter valid numbers');
+  if (!isValid) console.log('One of number is not valid. Please enter valid numbers\nNote: a, b and c have to be numbers, a must not be zero');
   else {
     console.log(`Equation is: ${a}x^2 + ${b}x ${ c >= 0 ? '+' : '-' } ${Math.abs(c)} = 0`);
     solveEquation(a, b, c);
