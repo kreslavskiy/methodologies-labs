be4daa9715ed8bc79566388a2b303602d539cee1
kreslavskiy <mishak1000236@gmail.com>
2023-04-03 14:59:42 +0300

    Added error handling

--- 3-lab/1-python/spaceship/routers/api.py
+++ 3-lab/1-python/spaceship/routers/api.py
@@ -10,14 +10,28 @@ def hello_world() -> dict:
 
 @router.get('/matrix')
 def matrix() -> dict:
-    first_matrix = numpy.random.rand(10, 10)
-    second_matrix = numpy.random.rand(10, 10)
-    result = numpy.dot(first_matrix, second_matrix)
+    first_rows = 10
+    first_columns = 11
 
-    loh = {
-        "first_matrix": first_matrix.tolist(),
-        "second_matrix": second_matrix.tolist(),
-        "result": result.tolist()
-    }
+    second_rows = 11
+    second_columns = 10
 
-    return loh
+    if first_columns != second_rows:
+
+        response = {
+            'result': 'Error: The number of columns of the first matrix must be equal to the number of rows of the second matrix.'
+        }
+
+    else:
+
+        first_matrix = numpy.random.rand(first_rows, first_columns)
+        second_matrix = numpy.random.rand(second_rows, second_columns)
+        result = numpy.dot(first_matrix, second_matrix)
+
+        response = {
+            "first_matrix": first_matrix.tolist(),
+            "second_matrix": second_matrix.tolist(),
+            "result": result.tolist()
+        }
+        
+    return response
